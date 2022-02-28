bd701cdfb7c1011dc313f581b30705d1466b570c
Pablo Cantillo <pablonahuelcantillo@gmail.com>
2022-02-28 03:08:22 -0300

    2do Desafio Terminado

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return <footer>
+        <a href="#">Instagram</a>
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Whatsapp</a>
+    </footer>
+}
+
+export default Footer 
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return <header>
+        <h1>Bienvenidos</h1>
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Productos</a>
+            <a href="#">Carrito</a>
+        </nav>
+    </header>
+}
+
+export default NavBar
