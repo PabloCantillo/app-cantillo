6c8b3a80f476bd1d1730ac0df819469149ae5380
Pablo Cantillo <pablonahuelcantillo@gmail.com>
2022-03-30 23:58:20 -0300

    12vo Desafio completado

--- src/index.js
+++ src/index.js
@@ -4,20 +4,6 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDEoRBFeBi2KJdmhfY0neAJSkM1iNX3e-8",
-  authDomain: "optica-cantillo.firebaseapp.com",
-  projectId: "optica-cantillo",
-  storageBucket: "optica-cantillo.appspot.com",
-  messagingSenderId: "753058289611",
-  appId: "1:753058289611:web:59f8a46f50540e07f3c84e"
-};
-
-const app = initializeApp(firebaseConfig);
-export const dbFirebase = getFirestore(app)
 
 ReactDOM.render(
   <React.StrictMode>
