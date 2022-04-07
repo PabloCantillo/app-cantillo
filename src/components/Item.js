ab802fa4c4a29576c535650fcb4dac7173525197
Pablo Cantillo <pablonahuelcantillo@gmail.com>
2022-04-06 21:24:11 -0300

    12do Desafio Corregido

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,21 +5,21 @@ import './Item.css';
 import { Link } from 'react-router-dom';
 
 const Item = ({productosInicial}) => {
-  console.log(productosInicial)
-    return (
-        
-            <Card className="cardList" style={{ width: '15rem' }}>
-                <Card.Img variant="top" src={productosInicial.img} />
-                <Card.Body>
-                  <Card.Title>{productosInicial.name}</Card.Title>
-                  <Card.Text>
-                    ${productosInicial.precio}
-                  </Card.Text>
-                  <Link to={`/item/${productosInicial.id}`}><Button variant="success">Ver mas</Button></Link>{' '}
-                </Card.Body>
-            </Card>
-        
-    )
+
+  return (
+      
+          <Card className="cardList" style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={productosInicial.productos.img} />
+              <Card.Body>
+                <Card.Title>{productosInicial.productos.name}</Card.Title>
+                <Card.Text>
+                  ${productosInicial.productos.precio}
+                </Card.Text>
+                <Link to={`/item/${productosInicial.id}`}><Button variant="success">Ver mas</Button></Link>{' '}
+              </Card.Body>
+          </Card>
+      
+  )
 }
 
 export default Item;
